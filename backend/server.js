a3f4b086d674365d9b5ef64c7d637fdd2b27f766
yangmunseok <alex2827@office.kw.ac.kr>
2025-10-09 20:34:27 +0900

    enable trust proxy to handle HTTPS redirect correctly on Render

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ const __dirname = path.resolve();
 
 passportConfig();
 
+app.enable("trust proxy");
+
 app.use(logger("dev"));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
